8d7c3e34e8fdcfddea1c5b90d254d96bcb04eca4
Fvbvke <novoselcevila12@gmail.com>
2023-09-09 11:49:44 +0300

    Update converter_callback.py

--- handlers/callbacks/converter_callback.py
+++ handlers/callbacks/converter_callback.py
@@ -2,6 +2,7 @@ from aiogram import Router, F
 from aiogram.types import CallbackQuery, FSInputFile
 from aiogram.filters.callback_data import CallbackData
 from utils.photo_converter import PhotoConverter
+from utils.audio_converter import AudioConverter
 from pathlib import Path
 from config import CONFIG
 import main
@@ -38,3 +39,30 @@ async def process_image_convert(callback: CallbackQuery,
 
     await callback.answer()
     os.remove(file_on_disk)
+    os.remove(converter.output_path)
+
+
+@router.callback_query(
+    FormatCallback.filter(F.type.in_(['AUDIO']))
+)
+async def process_image_convert(callback: CallbackQuery,
+                                callback_data: FormatCallback):
+    await callback.message.edit_text(text="Working...")
+
+    file_id = callback.message.reply_to_message.audio.file_id
+    file = await main.bot.get_file(file_id)
+    file_path = file.file_path
+    file_on_disk = Path(f"{CONFIG.media_full_path}{file_id}.{file_path.split('.')[-1]}")
+    await main.bot.download_file(file_path, destination=file_on_disk)
+
+    # convertation
+    converter = AudioConverter(file_on_disk.__str__(),
+                               song_name=callback.message.reply_to_message.audio.file_name)
+    await converter.convert_audio(callback_data.format)
+
+    await main.bot.send_audio(chat_id=callback.from_user.id,
+                              audio=FSInputFile(converter.output_path))
+
+    await callback.answer()
+    os.remove(file_on_disk)
+    os.remove(converter.output_path)
