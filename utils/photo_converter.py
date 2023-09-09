e7f61333fe5081e083b0b61479e6d1c94d691801
Fvbvke <novoselcevila12@gmail.com>
2023-09-09 11:48:55 +0300

    Fix

--- utils/photo_converter.py
+++ utils/photo_converter.py
@@ -5,7 +5,7 @@ class PhotoConverter:
     def __init__(self, path: str):
         self.input_path = path
         self.output_path = ""
-        self.image = image = Image.open(path)
+        self.image = Image.open(path)
 
     async def convert_photo(self, output_format):
         self.output_path = f"{self.input_path.split('.')[0]}.{output_format}"
