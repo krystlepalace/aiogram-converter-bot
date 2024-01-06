d618e3b8f19dcf31d04ee85505053463aac46ffc
Fvbvke <novoselcevila12@gmail.com>
2024-01-06 22:16:27 +0300

    Update media_handler.py

--- handlers/media_handler.py
+++ handlers/media_handler.py
@@ -1,6 +1,6 @@
 from aiogram import Router, F
 from aiogram.types import Message
-from keyboards.choose_formats import photo_formats_builder, audio_formats_builder, video_formats_builder
+from keyboards.choose_formats import formats_keyboard_builder
 
 
 router = Router()
@@ -9,16 +9,16 @@ router = Router()
 @router.message(F.photo)
 async def convert_photo(message: Message):
     await message.reply("Formats: ",
-                        reply_markup=photo_formats_builder().as_markup())
+                        reply_markup=formats_keyboard_builder(format_name="PHOTO").as_markup())
 
 
 @router.message(F.audio or F.voice)
-async def convert_photo(message: Message):
+async def convert_audio(message: Message):
     await message.reply("Formats: ",
-                        reply_markup=audio_formats_builder().as_markup())
+                        reply_markup=formats_keyboard_builder(format_name="AUDIO").as_markup())
 
 
 @router.message(F.video)
-async def convert_photo(message: Message):
+async def convert_video(message: Message):
     await message.reply("Formats: ",
-                        reply_markup=video_formats_builder().as_markup())
+                        reply_markup=formats_keyboard_builder(format_name="VIDEO").as_markup())
