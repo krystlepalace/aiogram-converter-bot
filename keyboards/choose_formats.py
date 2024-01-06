7961e8c7de6531cefb96ddfec1aca98edf976882
Fvbvke <novoselcevila12@gmail.com>
2024-01-06 22:14:40 +0300

    Update choose_formats.py
    Refactored keyboard builders

--- keyboards/choose_formats.py
+++ keyboards/choose_formats.py
@@ -2,45 +2,25 @@ from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 from handlers.callbacks.converter_callback import FormatCallback
 
-audio_formats = ['MP3', 'WAV', 'OGG',
-                 'FLAC', 'M4A', 'AAC',
-                 'AMR', 'OPUS', 'AIFF']
-photo_formats = ['PNG', 'JPG', 'JPEG',
-                 'BMP', 'GIF', 'WEBP',
-                 'SVG', 'ICO', 'TIFF']
-video_formats = ["MP4", "MP3", "GIF",
-                 "AVI", "MOV", "WEBM",
-                 "M4A", "MPEG", "WMV"]
 
-#refactor 3 builders into one
-def audio_formats_builder():
-    builder = InlineKeyboardBuilder()
-    for format in audio_formats:
-        builder.button(text=format, callback_data=FormatCallback(
-            format=format.lower(), type='AUDIO'
-        ).pack())
-    builder.adjust(3)
+MEDIA_FORMATS = {
+  "AUDIO": ['MP3', 'WAV', 'OGG',
+            'FLAC', 'M4A', 'AAC',
+            'AMR', 'OPUS', 'AIFF'],
+  "PHOTO": ['PNG', 'JPG', 'JPEG',
+            'BMP', 'GIF', 'WEBP',
+            'SVG', 'ICO', 'TIFF'],
+  "VIDEO": ["MP4", "MP3", "GIF",
+            "AVI", "MOV", "WEBM",
+            "M4A", "MPEG", "WMV"]
+}
 
-    return builder
+def formats_keyboard_builder(format_name):
+  builder = InlineKeyboardBuilder()
+  for format in FORMATS[format_name]:
+      builder.button(text=format, callback_data=FormatCallback(
+          format=format.lower(), type=format_name
+      ).pack())
+  builder.adjust(3)
 
-
-def photo_formats_builder():
-    builder = InlineKeyboardBuilder()
-    for format in photo_formats:
-        builder.button(text=format, callback_data=FormatCallback(
-            format=format.lower(), type='PHOTO'
-        ).pack())
-    builder.adjust(3)
-
-    return builder
-
-
-def video_formats_builder():
-    builder = InlineKeyboardBuilder()
-    for format in video_formats:
-        builder.button(text=format, callback_data=FormatCallback(
-            format=format.lower(), type='VIDEO'
-        ).pack())
-    builder.adjust(3)
-
-    return builder
+  return builder
